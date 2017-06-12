7657a19def76fab461184dfbb939075510cf616e
Jodi Parker <jodilparker@gmail.com>
2017-06-12 17:46:23 -0600

    routes hooked up

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -1,10 +1,58 @@
 'use strict';
 
-angular.module('app', []);
+angular.module('app', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/', "");
+    $stateProvider.state('home', {
+        templateUrl: '../views/home.html',
+        url: '/'
+    }).state('hcp_home', {
+        templateUrl: '../views/landing_page_hcp.html',
+        url: '/hcp_home'
+    }).state('user_home', {
+        templateUrl: '../views/landing_page_user.html',
+        url: '/user_home'
+    }).state('global_search', {
+        templateUrl: '../views/global_search_results.html',
+        url: '/global_search'
+    }).state('settings', {
+        templateUrl: '../views/settings.html',
+        url: '/settings'
+    }).state('test_create', {
+        templateUrl: '../views/test_create.html',
+        url: '/test_create'
+    }).state('test_manage', {
+        templateUrl: '../views/test_manage.html',
+        url: '/test_manage'
+    }).state('user_create', {
+        templateUrl: '../views/user_create.html',
+        url: '/user_create'
+    }).state('user_manage', {
+        templateUrl: '../views/user_manage.html',
+        url: '/user_manage'
+    });
+});
+"use strict";
+"use strict";
+"use strict";
 'use strict';
 
 angular.module('app').controller('mainCtrl', function ($scope) {
     // ...tests
     $scope.controllerTest = 'Controller Engaged!!!';
 });
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
 //# sourceMappingURL=bundle.js.map
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const massive = require('massive')
 const cors = require('cors')
 const Auth0Strategy = require('passport-auth0')
 const passport = require('passport')
+const config = require('./.config')
 
 //  =================   REQUIRE FILES
 const productsCtrl = require('./controllers/productsCtrl')
@@ -13,7 +14,7 @@ const productsCtrl = require('./controllers/productsCtrl')
 //  =================   OTHER VARIABLES
 const port = 3000
 
-//  =================   USE MODULES
+//  =================   MIDDLEWARE
 const app = express()
 app.use(express.static('../public'))
 // app.use(express.static(__dirname + '/public'))
@@ -29,9 +30,56 @@ app.use(bodyParser.json())
 //     console.log('connected to db')
 // });
 
-//  =================   ENDPOINTS
-// example: app.get('/products/:id', productsCtrl.getProductByID)
+// ...session setups
+app.use(session({
+  resave: true, //Without this you get a constant warning about default values
+  saveUninitialized: true, //Without this you get a constant warning about default values
+  secret: config.secret
+}))
+
+// ...passport setups
+app.use(passport.initialize())
+app.use(passport.session())
+
+// ...make the strategy (still part of passport setups)
+var strategy = new Auth0Strategy({
+  domain: config.domain,
+  clientID: config.clientID,
+  clientSecret: config.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/callback'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  return done(null, profile);
+});
+
+// ================ INVOKE PASSPORT METHODS
+// ...pass in the stategy
+passport.use(strategy);
+
+// ...serialize user data
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+// ...deserialize user data
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+//  ================= ENDPOINTS
 app.get('/api/products', productsCtrl.getTestProducts)
+
+// ...authorization endpoints
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback',
+  passport.authenticate('auth0', {successRedirect: '/#!/hcp_home', failureRedirect: '/login'}), function(req, res) {
+    res.status(200).send(req.user);
+})
+
+app.get('/auth/me', function(req, res) {
+  if (!req.user) return res.sendStatus(404);
+  res.status(200).send(req.user);
+})
 
 //  =================   TESTS
 app.listen(port, function(){
